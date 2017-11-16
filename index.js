aeda50ae74994649144d4dfa38a28e36fb7fbf48
Jose Tapia <jose.rikleo.tapia@gmail.com>
2017-11-16 22:45:51 +0100

    Mongoose Part 3

--- index.js
+++ index.js
@@ -19,10 +19,27 @@ connect.then((db) => {
     .then((dish) => {
         console.log(dish);
 
-        return Dishes.find({}).exec();
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'},
+        },
+        { 
+            new: true 
+        })
+        .exec();
     })
-    .then((dishes) => {
-        console.log(dishes);
+    .then((dish) => {
+        console.log(dish);
+
+        dish.comments.push({
+            rating: 5,
+            comment: 'I am getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
+
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
 
         return db.collection('dishes').drop();
     })
@@ -32,5 +49,5 @@ connect.then((db) => {
     .catch((err) => {
         console.log(err);
     });
-    
+
 });
